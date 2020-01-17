5a857542d5ae80b19ec1ef6acdbb2f1df52a82b4
Michał Walkowski <mi.walkowski@gmail.com>
2020-01-17 16:07:16 +0100

    demodata create elastalert index

--- demo/config/demo_data.sh
+++ demo/config/demo_data.sh
@@ -5,6 +5,9 @@ echo "Ralph: Load demo data  (login: ralph, password: ralph)"
 docker exec -it compose_web_1 ralphctl demodata
 docker exec -it compose_web_1 python3 /test_data/generate_ips.py
 
+echo "Elastalert: Create Indexes"
+docker exec -it compose_elastalert_1 /usr/bin/elastalert-create-index --config /opt/elastalert/config.yaml
+
 echo "VMC: Load data (superuser login: admin, password:adminadmin)"
 docker exec -it compose_admin_1 vmc loaddata /test_data/demo_data.json
 
@@ -13,5 +16,3 @@ docker exec -it compose_kibana_1 /test_data/load.sh
 
 echo "VMC: Import Assets from Ralph"
 docker exec -it compose_admin_1 python3 -W ignore /test_data/generate_vulns.py
-
-echo "Done"
