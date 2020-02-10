4a5e18eb0a54521b7fbf55fe8129ff8caf144b76
Michał Walkowski <mi.walkowski@gmail.com>
2020-02-10 20:56:07 +0100

    update documents

--- demo/config/demo_data.sh
+++ demo/config/demo_data.sh
@@ -16,8 +16,8 @@ echo "VMC: Load data (superuser login: admin, password: adminadmin)"
 docker exec -it compose_admin_1 vmc loaddata /test_data/demo_data.json
 
 echo "Kibana: Import Sample Dashboards and KPIs"
-docker exec -it compose_kibana_1 chmod +x /test_data/load.sh
-docker exec -it compose_kibana_1 /test_data/load.sh
+#docker exec -it compose_kibana_1 chmod +x /test_data/load.sh
+#docker exec -it compose_kibana_1 /test_data/load.sh
 
 echo "VMC: Import Assets from Ralph"
 docker exec -it compose_admin_1 python3 -W ignore /test_data/generate_vulns.py
